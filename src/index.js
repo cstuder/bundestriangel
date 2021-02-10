b4d6c27f8a0e35c25ea5ba748599bd954cd5756a
Christian Studer <cstuder@existenz.ch>
2021-02-10 23:06:34 +0100

    Upload: Working

--- src/index.js
+++ src/index.js
@@ -1,38 +1,54 @@
-import 'bootstrap'
-import './index.scss'
+import "bootstrap";
+import "./index.scss";
 
 const CANVAS_WIDTH = 730;
 const CANVAS_HEIGHT = 383;
+const TARGET_X = 150;
+const TARGET_Y = 234;
+const TARGET_W = 465;
+const TARGET_H = 80;
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    const inputElement = document.getElementById("upload");
-    inputElement.addEventListener("change", handleUpload, false);
+window.onload = (event) => {
+  const inputElement = document.getElementById("upload");
+  inputElement.addEventListener("change", handleUpload, false);
 
-    loadInitialImage();
-});
+  loadInitialImage();
+};
 
 function loadInitialImage() {
-    var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
-    ctx.drawImage(document.getElementById('initialImage'), 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+  const initialImage = document.getElementById("initialImage");
+
+  ctx.drawImage(initialImage, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 }
 
 function handleUpload() {
-    const image = new Image();
-    image.onload = draw;
-    image.onerror = failed;
-    image.src = URL.createObjectURL(this.files[0]);
+  const errorBox = document.getElementById("error");
+  errorBox.classList.add("d-none");
+
+  const image = new Image();
+  image.onload = drawOnTop;
+  image.onerror = failed;
+  image.src = URL.createObjectURL(this.files[0]);
 }
 
-function draw() {
-  var canvas = document.getElementById('canvas');
-  canvas.width = this.width;
-  canvas.height = this.height;
-  var ctx = canvas.getContext('2d');
-  ctx.drawImage(this, 0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
+function drawOnTop() {
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+
+  // Take full width, but put it in the middle
+  const shrinkRatio = TARGET_W / this.width;
+  const newHeight = this.height * shrinkRatio;
+  const newY = TARGET_Y + TARGET_H / 2 - newHeight / 2;
+
+  ctx.drawImage(this, TARGET_X, newY, TARGET_W, newHeight);
+
+  const initialImage = document.getElementById("initialImage");
+  ctx.drawImage(initialImage, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 }
 
 function failed() {
-    console.log(this);
-    console.error("The provided file couldn't be loaded as an Image media");
+  const errorBox = document.getElementById("error");
+  errorBox.classList.remove("d-none");
 }
