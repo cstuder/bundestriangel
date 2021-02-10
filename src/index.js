726467e64b943cfe97192e1d7c5f29e5f3437f62
Christian Studer <cstuder@existenz.ch>
2021-02-10 08:45:53 +0100

    Canvas: WIP

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import 'bootstrap'
-import '@fortawesome/fontawesome-free/css/all.css'
 import './index.scss'
 
-document.addEventListener('DOMContentLoaded', (event) => {
-  const inputElement = document.getElementById("upload");
-  inputElement.addEventListener("change", handleUpload, false);
+const CANVAS_WIDTH = 730;
+const CANVAS_HEIGHT = 383;
 
-})
+document.addEventListener('DOMContentLoaded', (event) => {
+    const inputElement = document.getElementById("upload");
+    inputElement.addEventListener("change", handleUpload, false);
+
+    loadInitialImage();
+});
+
+function loadInitialImage() {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    ctx.drawImage(document.getElementById('initialImage'), 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
 
 function handleUpload() {
     const image = new Image();
@@ -20,9 +29,10 @@ function draw() {
   canvas.width = this.width;
   canvas.height = this.height;
   var ctx = canvas.getContext('2d');
-  ctx.drawImage(this, 0,0);
+  ctx.drawImage(this, 0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
 }
 
 function failed() {
-  console.error("The provided file couldn't be loaded as an Image media");
+    console.log(this);
+    console.error("The provided file couldn't be loaded as an Image media");
 }
